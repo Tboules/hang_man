704c31f77fa00e4f9ed6cc7e6caddcbfa7527dbf
Tboules <tboules@gmail.com>
2023-11-14 19:38:02 -0800

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import Layout from "./components/Layout";
 import Person from "./components/Person";
 import useGameLogic from "./hooks/useGameLogic";
 
 function App() {
   const { increaseStageToDeath, stageToDeath } = useGameLogic();
   return (
-    <div className="p-10">
+    <Layout>
       <Person stage={stageToDeath} />
       <button onClick={increaseStageToDeath}> kill the person </button>
-    </div>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren } from "react";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className="flex w-full justify-center items-center flex-col p-10 max-w-screen-lg m-auto">
+      {children}
+    </div>
+  );
+}
