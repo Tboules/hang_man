90263935a762dddf023c1c61adddbc32c36e8473
Tboules <tboules@gmail.com>
2023-11-15 13:31:42 -0800

    gallow

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,24 +1,40 @@
+import { PropsWithChildren } from "react";
+
 interface IPersonProps {
   stage: number;
 }
 
 export default function Person({ stage }: IPersonProps) {
   return (
-    <div className="relative flex flex-col items-center w-32">
-      {stage > 0 && <Head />}
-      {stage > 1 && <Body />}
-      {stage > 2 && <Arm bodyPart={{ type: "arm", side: "right" }} />}
-      {stage > 3 && <Arm bodyPart={{ type: "arm", side: "left" }} />}
+    <Gallow>
+      <div className="absolute top-16 right-[-60px]">
+        <div className="relative flex flex-col items-center w-32">
+          {stage > 0 && <Head />}
+          {stage > 1 && <Body />}
+          {stage > 2 && <Arm bodyPart={{ type: "arm", side: "right" }} />}
+          {stage > 3 && <Arm bodyPart={{ type: "arm", side: "left" }} />}
 
-      <div className="flex">
-        <Leg
-          styles={stage > 4 ? "" : "invisible"}
-          bodyPart={{ type: "leg", side: "left" }}
-        />
-        <Leg
-          styles={stage > 5 ? "" : "invisible"}
-          bodyPart={{ type: "leg", side: "right" }}
-        />
+          <div className="flex">
+            <Leg
+              styles={stage > 4 ? "" : "invisible"}
+              bodyPart={{ type: "leg", side: "left" }}
+            />
+            <Leg
+              styles={stage > 5 ? "" : "invisible"}
+              bodyPart={{ type: "leg", side: "right" }}
+            />
+          </div>
+        </div>
+      </div>
+    </Gallow>
+  );
+}
+
+function Gallow({ children }: PropsWithChildren) {
+  return (
+    <div className="p-4">
+      <div className="relative before:absolute before:w-72 before:h-2 before:bottom-0 before:right-16 before:bg-black after:w-2 after:h-16 after:bg-black after:absolute after:right-0 after:top-0 w-64 h-96 border-t-8 border-l-8 border-black">
+        {children}
       </div>
     </div>
   );
@@ -75,7 +91,7 @@ function Arm({ bodyPart }: { bodyPart: IBodyPart }) {
     <>
       <div
         className={`w-12 h-2 bg-black absolute top-20 ${determineStylesBasedOnBodyPart(
-          bodyPart,
+          bodyPart
         )}`}
       />
     </>
@@ -87,7 +103,7 @@ function Leg({ bodyPart, styles }: { bodyPart: IBodyPart; styles: string }) {
     <>
       <div
         className={`h-14 w-2 bg-black -translate-y-2 ${determineStylesBasedOnBodyPart(
-          bodyPart,
+          bodyPart
         )} ${styles}`}
       />
     </>
